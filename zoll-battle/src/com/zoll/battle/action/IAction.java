d9b9dd0f232a5a352ce4f537c9218829bf7dccdd
lostFatRivers <495669631@qq.com>
2015-11-17 23:50:28 +0800

    start Swing

---
+++ zoll-battle/src/com/zoll/battle/action/IAction.java
@@ -0,0 +1,11 @@
+package com.zoll.battle.action;
+
+/**
+ * 作用在role上;
+ * 
+ * @author joker
+ *
+ */
+public interface IAction {
+	
+}
---
+++ zoll-battle/src/com/zoll/battle/role/IRole.java
@@ -0,0 +1,14 @@
+package com.zoll.battle.role;
+
+/**
+ * 角色接口;
+ * 
+ * @author joker
+ *
+ */
+public interface IRole {
+	/**
+	 * 行为执行;
+	 */
+	public void doAction();
+}
